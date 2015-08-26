3a796bb8883f7b32f8833db6f0e15548e8f7dd9c
Ivan Wills <ivan.wills@gmail.com>
2015-08-26 17:16:02 +1000

    Updating for release of version 0.0.3

--- lib/Group/Git/Cmd/SinceRelease.pm
+++ lib/Group/Git/Cmd/SinceRelease.pm
@@ -16,7 +16,7 @@ use English qw/ -no_match_vars /;
 use File::chdir;
 use Getopt::Alt;
 
-our $VERSION = version->new('0.0.2');
+our $VERSION = version->new('0.0.3');
 
 my $opt = Getopt::Alt->new(
     {
@@ -87,7 +87,7 @@ Group::Git::Cmd::SinceRelease - Gets the number of commits each repository is ah
 
 =head1 VERSION
 
-This documentation refers to Group::Git::Cmd::SinceRelease version 0.0.2
+This documentation refers to Group::Git::Cmd::SinceRelease version 0.0.3
 
 =head1 SYNOPSIS
 
